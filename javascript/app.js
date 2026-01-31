1771c591f5160bcedfb871c32534648df0550c23
Eva Mothapo <matlalaeva6@gmail.com>
2026-01-31 20:14:25 +0200

    Added the functionality for Select

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,6 @@
 function updateTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
+
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
   let losAngelesTimeElement = losAngelesElement.querySelector(".time");
   let losAngelesTime = moment().tz("America/Los_Angeles");
@@ -18,3 +19,19 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("Do MMM YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+</div>`;
+}
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
